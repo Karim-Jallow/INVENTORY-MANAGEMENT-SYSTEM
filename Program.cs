1dc97e5c7c7688538c01e4ed66b11b738d1ccce7
Karim-Jallow <abdoukarimj@hotmail.com>
2024-10-12 20:49:07 +0000

    Initial commit of INVENTORY MANAGEMENT SYSTEM

--- Program.cs
+++ Program.cs
@@ -2,13 +2,22 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using INVENTORY_MANAGEMENT_SYSTEM.Models;
 using TVET_MANAGEMENT_SYSTEM.Data;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure; // Make sure to include this namespace
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(8, 0, 23)))); // Adjust the version as needed
+        new MySqlServerVersion(new Version(8, 0, 23)), // Adjust the version as needed
+        mysqlOptions =>
+        {
+            mysqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5, // Number of retry attempts
+                maxRetryDelay: TimeSpan.FromSeconds(30), // Delay between retries
+                errorNumbersToAdd: null // Add specific error numbers if needed
+            );
+        }));
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
